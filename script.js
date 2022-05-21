abc1e5d73d761e136043fefc58100c7691649c75
naknamu <jodeldelvalle@gmail.com>
2022-05-21 12:38:27 +0800

    Added player objects and function to check winner

--- script.js
+++ script.js
@@ -1,42 +1,138 @@
 const gameBoard = (() => {
-    let myArray= [
-        [0,0], 
-        [0,1],
-        [0,2],
-        [1,0],
-        [1,1],
-        [1,2],
-        [2,0],
-        [2,1],
-        [2,2]
-    ];
-    let count = 0;
-    const createBoard = () => {
-        for (let i=0; i<3; i++) {
-            for (let j=0; j<3; j++) {
-                 myArray[i][j] = count++;
-                 console.log(myArray[i][j]);
-            }
-        }
-    }
-    return {createBoard};
+    // let myArray= [
+    //     [0,0], 
+    //     [0,1],
+    //     [0,2],
+    //     [1,0],
+    //     [1,1],
+    //     [1,2],
+    //     [2,0],
+    //     [2,1],
+    //     [2,2]
+    // ];
+    let myArray = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    // let count = 0;
+    // const createBoard = () => {
+    //     for (let i=0; i<3; i++) {
+    //         for (let j=0; j<3; j++) {
+    //              myArray[i][j] = count++;
+    //              console.log(myArray[i][j]);
+    //         }
+    //     }
+    // }
+    return {myArray};
 })();
+
+//player game objects
+const playerOne = {
+    name: 'Player One',
+    marker: 'X',
+    turn: true
+}
+
+const playerTwo = {
+    name: 'Player Two',
+    marker: 'O',
+    turn: false
+}
 
 //locate the gameboard tiles in the html
 const boardTiles = document.querySelectorAll('.gameBoard>div');
 
 boardTiles.forEach((boardTile)=> {
     boardTile.addEventListener('click', () => {
-        //
+        //player taking turns in playing
+        if (playerOne.turn)
+        {
+            boardTile.textContent = playerOne.marker;
+            playerOne.turn = false;
+            playerTwo.turn = true;
+        } 
+        else {
+            boardTile.textContent = playerTwo.marker;
+            playerTwo.turn = false;
+            playerOne.turn = true;
+        }
+        gameBoard.myArray[boardTile.id] = boardTile.textContent;
+        console.log(gameBoard.myArray[boardTile.id]);
+        winner.check();
     })
 })
 
-//player 1 logic
-const playerOne = (input) => {
-    return {input};
-}
+//function to check winner
+const winner = (() => {
+    const check = () => {
+        //PLAYER ONE ROWS
+        if (gameBoard.myArray[0] == playerOne.marker && gameBoard.myArray[1] == playerOne.marker && gameBoard.myArray[2] == playerOne.marker){
+            console.log(playerOne.name + ' WON!');
+            //call gameover fucntion
+        }
+        else if (gameBoard.myArray[3] == playerOne.marker && gameBoard.myArray[4] == playerOne.marker && gameBoard.myArray[5] == playerOne.marker){
+            console.log(playerOne.name + ' WON!');
+            //call gameover fucntion
+        }
+        else if (gameBoard.myArray[6] == playerOne.marker && gameBoard.myArray[7] == playerOne.marker && gameBoard.myArray[8] == playerOne.marker){
+            console.log(playerOne.name + ' WON!');
+            //call gameover fucntion
+        }
+        //PLAYER TWO ROWS
+        if (gameBoard.myArray[0] == playerTwo.marker && gameBoard.myArray[1] == playerTwo.marker && gameBoard.myArray[2] == playerTwo.marker){
+            console.log(playerTwo.name + ' WON!');
+            //call gameover fucntion
+        }
+        else if (gameBoard.myArray[3] == playerOne.marker && gameBoard.myArray[4] == playerOne.marker && gameBoard.myArray[5] == playerOne.marker){
+            console.log(playerTwo.name + ' WON!');
+            //call gameover fucntion
+        }
+        else if (gameBoard.myArray[6] == playerOne.marker && gameBoard.myArray[7] == playerOne.marker && gameBoard.myArray[8] == playerOne.marker){
+            console.log(playerTwo.name + ' WON!');
+            //call gameover fucntion
+        }
+        //PLAYER ONE COLUMN
+        if (gameBoard.myArray[0] == playerOne.marker && gameBoard.myArray[3] == playerOne.marker && gameBoard.myArray[6] == playerOne.marker){
+            console.log(playerOne.name + ' WON!');
+            //call gameover fucntion
+        }
+        else if (gameBoard.myArray[1] == playerOne.marker && gameBoard.myArray[4] == playerOne.marker && gameBoard.myArray[7] == playerOne.marker){
+            console.log(playerOne.name + ' WON!');
+            //call gameover fucntion
+        }
+        else if (gameBoard.myArray[2] == playerOne.marker && gameBoard.myArray[5] == playerOne.marker && gameBoard.myArray[8] == playerOne.marker){
+            console.log(playerOne.name + ' WON!');
+            //call gameover fucntion
+        }
+        //PLAYER TWO COLUMN
+        if (gameBoard.myArray[0] == playerTwo.marker && gameBoard.myArray[3] == playerTwo.marker && gameBoard.myArray[6] == playerTwo.marker){
+            console.log(playerTwo.name + ' WON!');
+            //call gameover fucntion
+        }
+        else if (gameBoard.myArray[1] == playerTwo.marker && gameBoard.myArray[4] == playerTwo.marker && gameBoard.myArray[7] == playerTwo.marker){
+            console.log(playerTwo.name + ' WON!');
+            //call gameover fucntion
+        }
+        else if (gameBoard.myArray[2] == playerTwo.marker && gameBoard.myArray[5] == playerTwo.marker && gameBoard.myArray[8] == playerTwo.marker){
+            console.log(playerTwo.name + ' WON!');
+            //call gameover fucntion
+        }
+        //PLAYER ONE DIAGONAL
+        if (gameBoard.myArray[0] == playerOne.marker && gameBoard.myArray[4] == playerOne.marker && gameBoard.myArray[8] == playerOne.marker){
+            console.log(playerOne.name + ' WON!');
+            //call gameover fucntion
+        }
+        else if (gameBoard.myArray[2] == playerOne.marker && gameBoard.myArray[4] == playerOne.marker && gameBoard.myArray[6] == playerOne.marker){
+            console.log(playerOne.name + ' WON!');
+            //call gameover fucntion
+        }
+        //PLAYER TWO DIAGONAL
+        if (gameBoard.myArray[0] == playerTwo.marker && gameBoard.myArray[4] == playerTwo.marker && gameBoard.myArray[8] == playerTwo.marker){
+            console.log(playerTwo.name + ' WON!');
+            //call gameover fucntion
+        }
+        else if (gameBoard.myArray[2] == playerTwo.marker && gameBoard.myArray[4] == playerTwo.marker && gameBoard.myArray[6] == playerTwo.marker){
+            console.log(playerTwo.name + ' WON!');
+            //call gameover fucntion
+        }
 
-//player 2 logic
-const playerTwo = (input) => {
-    return {input};
-}
+    }
+    return {check};
+})();
