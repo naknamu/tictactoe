fa6d8591d5de070e09f8f50016f18842da2c2960
naknamu <jodeldelvalle@gmail.com>
2022-05-21 12:41:46 +0800

    Removed comments

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
+//gameboard object
 const gameBoard = (() => {
-    // let myArray= [
-    //     [0,0], 
-    //     [0,1],
-    //     [0,2],
-    //     [1,0],
-    //     [1,1],
-    //     [1,2],
-    //     [2,0],
-    //     [2,1],
-    //     [2,2]
-    // ];
     let myArray = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-    // let count = 0;
-    // const createBoard = () => {
-    //     for (let i=0; i<3; i++) {
-    //         for (let j=0; j<3; j++) {
-    //              myArray[i][j] = count++;
-    //              console.log(myArray[i][j]);
-    //         }
-    //     }
-    // }
     return {myArray};
 })();
 
